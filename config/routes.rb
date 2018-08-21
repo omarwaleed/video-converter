90fc0396084ca4be9f92d8f541df9435d2db04b5
omarwaleed <omarpersonalprofile@gmail.com>
2018-08-21 12:19:00 +0200

    fixing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
 
-  resource :video do
-    post 'check' => 'video#check'
-    post 'convert' => 'video#convert'
-  end
+  
+  post 'video/check' => 'video#check'
+  post 'video/convert' => 'video#convert'
   # get 'video/check'
   # get 'video/convert'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
